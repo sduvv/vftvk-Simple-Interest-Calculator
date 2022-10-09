35ef496a87eccf136e2ec651bb2c72227bcf8824
sduvv <115382628+sduvv@users.noreply.github.com>
2022-10-09 16:41:30 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,22 @@
+// Calculate simple interest and show message to the user
 function compute()
 {
-    p = document.getElementById("principal").value;
-    
+    var principal = document.getElementById("principal").value;
+    if(principal <= 0){
+        alert("Enter a positive number")
+    }else{
+        var rate = document.getElementById("rate").value;
+        var years = document.getElementById("years").value;
+
+        var year = new Date().getFullYear()+parseInt(years);
+        var amount = principal * rate * years / 100;
+        document.getElementById("result").innerHTML="If you deposit <mark>"+principal+"</mark>,\<br\>at an interest rate of <mark>"+rate+"</mark>%\<br\>You will receive an amount of <mark>"+amount+"</mark>,\<br\>in the year <mark>"+year+"</mark>\<br\>"
+    }
 }
-        
+
+// Update rate when the value changes to show the current % 
+function updateRate() 
+{
+    var rateval = document.getElementById("rate").value;
+    document.getElementById("rate_val").innerText=rateval;
+}
